eb7e324d51b9a7c7216cfb6c5e6a55defe32d27b
M KIR AHMET <mkirahmet@gmail.com>
2024-12-26 12:23:44 +0200

    drop down menu with genres

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const tmdbKey = '00c7e4fd0c0d13262594155fd01ee4c8';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = async () => {
+  const genreRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`
+  try{
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  }catch(errors){
+    console.log(errors);
+  }
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
